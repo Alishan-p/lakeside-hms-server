6509972d953abd6e5e5343794bc8980182ff1839
alishan-p <alishanummer@gmail.com>
2025-04-18 10:07:26 +0530

    initial commit

---
+++ src/main/java/com/springbootweb/hms_server/HmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.springbootweb.hms_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsServerApplication.class, args);
+	}
+
+}
