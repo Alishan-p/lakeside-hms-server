438d3afe14f61b20f4e3a6fed5f66c7983e18adc
alishan-p <alishanummer@gmail.com>
2025-04-18 12:06:35 +0530

    feat(model) added the room and bookedRoom entity

--- src/main/java/com/springbootweb/hms_server/model/BookedRoom.java
+++ src/main/java/com/springbootweb/hms_server/model/BookedRoom.java
@@ -1,5 +1,65 @@
 package com.springbootweb.hms_server.model;
 
-public class BookedRoom {
+import java.time.LocalDate;
+import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "booked_rooms")
+@Data
+public class BookedRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "check_in")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_out")
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_fullname")
+    private String guestFullName;
+
+    private String guestEmail;
+
+    @Column(name = "adults")
+    private int numOfAdults;
+
+    @Column(name = "childrens")
+    private int numOfChildrens;
+
+    @Column(name = "total_guests")
+    private int totalNumOfGuest;
+
+    @Column(name = "confirmation_code")
+    private UUID bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumberOfGuest() {
+        this.totalNumOfGuest = this.numOfAdults + this.numOfChildrens;
+    }
+
+    public void setNumOfAdults(int num) {
+        this.numOfAdults = num;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setNumOfChildrens(int num) {
+        this.numOfChildrens = num;
+        calculateTotalNumberOfGuest();
+    }
 }
--- src/main/java/com/springbootweb/hms_server/model/Room.java
+++ src/main/java/com/springbootweb/hms_server/model/Room.java
@@ -1,5 +1,54 @@
 package com.springbootweb.hms_server.model;
 
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "rooms")
+@Data
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked = false;
+
+    @Lob
+    private Blob photo;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<BookedRoom> bookings;
+
+    public Room() {
+        bookings = new ArrayList<>();
+    }
+
+    public void addBooking(BookedRoom booking) {
+        if (bookings == null) {
+            bookings = new ArrayList<>();
+        }
+
+        bookings.add(booking);
+        booking.setRoom(this);
+        isBooked = true;
+        UUID bookingCode = UUID.randomUUID();
+        booking.setBookingConfirmationCode(bookingCode);
+    }
 
 }
